cd0cfcdee38fed38d71da13f5d796d9cec185940
Yitong Sun <syitong@umich.edu>
2018-05-16 08:36:46 +0800

    dim 2 plot generated.

--- ResultPlot.py
+++ ResultPlot.py
@@ -48,7 +48,7 @@ def rate_plot(samplesize,opt_filename,unif_filename,image_filename):
     fig = plt.figure()
     plt.errorbar(samplesize,unif_err_rate,unif_std,fmt='g:x',fillstyle='none',label='unif')
     plt.errorbar(samplesize,opt_err_rate,opt_std,fmt='b--s',fillstyle='none',label='opt')
-    plt.yticks(np.arange(0,0.25,0.05))
+    plt.yticks(np.arange(0,0.2,0.05))
     plt.legend(loc=1)
     plt.xlabel('$\log(m)$')
     plt.ylabel('error rate')
@@ -60,21 +60,21 @@ def main():
     trials = range(1,11,1)
     folder = 'result/'
     prefix = 'opt_best_score trial '
-    suffix = ' dim 10'
+    suffix = ' dim 2'
     trials_agg(trials,folder,prefix,suffix)
 
     ### combine unif_best_score results from different trials
     trials = range(1,11,1)
     folder = 'result/'
     prefix = 'unif_best_score trial '
-    suffix = ' dim 10'
+    suffix = ' dim 2'
     trials_agg(trials,folder,prefix,suffix)
 
     ### plot the learning rate
     samplesize = np.arange(2,6,0.5)
-    opt_filename = 'result/opt_best_score trial  dim 10.csv'
-    unif_filename = 'result/unif_best_score trial  dim 10.csv'
-    image_filename = 'image/learningrate dim 10.eps'
+    opt_filename = 'result/opt_best_score trial  dim 2.csv'
+    unif_filename = 'result/unif_best_score trial  dim 2.csv'
+    image_filename = 'image/learningrate dim 2.eps'
     rate_plot(samplesize,opt_filename,unif_filename,image_filename)
 
     ### plot sample points
